b5c3b90da7c7f077b634f84e61daccc9f36239b3
jorgedr <jorgedr@us.ibm.com>
2018-04-02 23:26:00 -0400

    support timeout flag

--- cmds/chaincode-cmds/instantiate.js
+++ cmds/chaincode-cmds/instantiate.js
@@ -11,46 +11,64 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const hfc = require('fabric-client');
-const path = require('path');
-const instantiateLib = require('../../lib/instantiate-chaincode.js')
+const fs = require("fs");
+const hfc = require("fabric-client");
+const path = require("path");
+const instantiateLib = require("../../lib/instantiate-chaincode.js");
 
-exports.command = 'instantiate';
-exports.desc = 'Instantiate chaincode';
-exports.builder = function (yargs) {
-    return yargs.option('cc-name', {
-        demandOption: true,
-        describe: 'Name for the chaincode to instantiate',
-        requiresArg: true,
-        type: 'string'
-    }).option('cc-version', {
-        demandOption: true,
-        describe: 'The version of chaincode to instantiate',
-        requiresArg: true,
-        type: 'string'
-    }).option('channel', {
-        demandOption: true,
-        describe: 'Name of the channel to instantiate chaincode',
-        requiresArg: true,
-        type: 'string'
-    }).option('init-arg', {
-        array: true,
-        demandOption: false,
-        describe: 'Value(s) to pass as argument to instantiation call.',
-        requiresArg: false,
-        type: 'string',
-        default: []
+exports.command = "instantiate";
+exports.desc = "Instantiate chaincode";
+exports.builder = function(yargs) {
+  return yargs
+    .option("cc-name", {
+      demandOption: true,
+      describe: "Name for the chaincode to instantiate",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("cc-version", {
+      demandOption: true,
+      describe: "The version of chaincode to instantiate",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("channel", {
+      demandOption: true,
+      describe: "Name of the channel to instantiate chaincode",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("init-arg", {
+      array: true,
+      demandOption: false,
+      describe: "Value(s) to pass as argument to instantiation call.",
+      requiresArg: false,
+      type: "string",
+      default: []
+    })
+    .option("timeout", {
+      demandOption: false,
+      describe:
+        "Specify number of milliseconds to wait on the response before rejecting ",
+      requiresArg: true,
+      type: "number",
+      default: 120000
     });
 };
 
-exports.handler = function (argv) {
-
-    //let's get peers from config file
-    console.log("Instantiating chaincode");
-    return instantiateLib.instantiateChaincode(argv['net-config'], argv['channel'], 
-            argv['cc-name'], argv['cc-version'], argv['init-arg'], 
-            null, argv['org']);
+exports.handler = function(argv) {
+  //let's get peers from config file
+  console.log("Instantiating chaincode");
+  return instantiateLib.instantiateChaincode(
+    argv["net-config"],
+    argv["channel"],
+    argv["cc-name"],
+    argv["cc-version"],
+    argv["init-arg"],
+    null,
+    argv["org"],
+    argv["timeout"]
+  );
 };
--- cmds/chaincode-cmds/invoke.js
+++ cmds/chaincode-cmds/invoke.js
@@ -11,55 +11,78 @@ See the License for the specific language governing permissions and
 limitations under the License.
 */
 
-'use strict';
+"use strict";
 
-const fs = require('fs');
-const hfc = require('fabric-client');
-const path = require('path');
-const invokeLib = require('../../lib/invoke-transaction.js')
+const fs = require("fs");
+const hfc = require("fabric-client");
+const path = require("path");
+const invokeLib = require("../../lib/invoke-transaction.js");
 
-exports.command = 'invoke';
-exports.desc = 'Invoke a transaction in the chaincode';
-exports.builder = function (yargs) {
-    return yargs.option('cc-name', {
-        demandOption: true,
-        describe: 'Name for the chaincode to invoke',
-        requiresArg: true,
-        type: 'string'
-    }).option('cc-version', {
-        demandOption: true,
-        describe: 'The version of chaincode to invoke',
-        requiresArg: true,
-        type: 'string'
-    }).option('channel', {
-        demandOption: true,
-        describe: 'Name of the channel to invoke chaincode',
-        requiresArg: true,
-        type: 'string'
-    }).option('invoke-fn', {
-        demandOption: true,
-        describe: 'Name of the function to invoke in chaincode',
-        requiresArg: true,
-        type: 'string'
-    }).option('invoke-arg', {
-        array: true,
-        demandOption: false,
-        describe: 'Value(s) to pass as argument to invoke call.',
-        requiresArg: false,
-        type: 'string',
-        default: []
-    }).option('query', {
-        demandOption: false,
-        describe: 'Specify if this invocation is just a query to the ledger',
-        requiresArg: false,
-        type: 'boolean',
-        default: false
+exports.command = "invoke";
+exports.desc = "Invoke a transaction in the chaincode";
+exports.builder = function(yargs) {
+  return yargs
+    .option("cc-name", {
+      demandOption: true,
+      describe: "Name for the chaincode to invoke",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("cc-version", {
+      demandOption: true,
+      describe: "The version of chaincode to invoke",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("channel", {
+      demandOption: true,
+      describe: "Name of the channel to invoke chaincode",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("invoke-fn", {
+      demandOption: true,
+      describe: "Name of the function to invoke in chaincode",
+      requiresArg: true,
+      type: "string"
+    })
+    .option("invoke-arg", {
+      array: true,
+      demandOption: false,
+      describe: "Value(s) to pass as argument to invoke call.",
+      requiresArg: false,
+      type: "string",
+      default: []
+    })
+    .option("query", {
+      demandOption: false,
+      describe: "Specify if this invocation is just a query to the ledger",
+      requiresArg: false,
+      type: "boolean",
+      default: false
+    })
+    .option("timeout", {
+      demandOption: false,
+      describe:
+        "Specify number of milliseconds to wait on the response before rejecting ",
+      requiresArg: true,
+      type: "number",
+      default: 120000
     });
 };
 
-exports.handler = function (argv) {
-
-    //let's get peers from config file
-    console.log("Invoking transaction in chaincode");
-    return invokeLib.invokeTransaction(argv['net-config'], argv['channel'], argv['cc-name'], argv['cc-version'], argv['invoke-arg'],  argv['invoke-fn'],argv['org'], argv['query']);
+exports.handler = function(argv) {
+  //let's get peers from config file
+  console.log("Invoking transaction in chaincode");
+  return invokeLib.invokeTransaction(
+    argv["net-config"],
+    argv["channel"],
+    argv["cc-name"],
+    argv["cc-version"],
+    argv["invoke-arg"],
+    argv["invoke-fn"],
+    argv["org"],
+    argv["query"],
+    argv["timeout"]
+  );
 };
