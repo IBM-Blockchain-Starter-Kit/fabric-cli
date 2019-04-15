8d7be229e7c2cabfabf3c655f4ad6fd07f881260
Joshua Vines <jvines@uk.ibm.com>
2019-04-15 16:33:56 +0100

    Added tests for fabricHelper static methods

--- src/FabricHelper.spec.ts
+++ src/FabricHelper.spec.ts
@@ -10,8 +10,185 @@ const EXAMPLE_UNKNOWN_ORG = 'orgUnknown';
 const EXAMPLE_CHANNEL_NAME = 'examplechannel';
 const EXAMPLE_KEY_VALUE_STORE_BASE_PATH = `${__dirname}/../testData`;
 const EXAMPLE_CRYPTO_DIR_PATH = `${__dirname}`;
+const EXAMPLE_ORG_MSP = 'exampleOrgMSP';
+
+const examplePeer1 = new FabricClient.Peer('grpc://peer1.example.com', {
+    name: 'peer1'
+});
+const examplePeer2 = new FabricClient.Peer('grpc://peer2.example.com', {
+    name: 'peer2'
+});
+
+const exampleProposalResponse1: any = {
+    response: { status: 200 }
+};
+const exampleProposalResponse2: any = {
+    response: { status: 200 }
+};
+
+const exampleErrorProposalResponse1: Error = new Error('Bad response');
+
+const exampleBadProposalResponse1: any = {
+    response: { status: 400 }
+};
+
+const exampleProposalResponses = [
+    [exampleProposalResponse1, exampleProposalResponse2]
+];
+
+const exampleProposalResponsesWithError = [
+    [
+        exampleProposalResponse1,
+        exampleErrorProposalResponse1,
+        exampleProposalResponse2
+    ]
+];
+
+const exampleProposalResponsesWithBad = [
+    [
+        exampleProposalResponse1,
+        exampleBadProposalResponse1,
+        exampleProposalResponse2
+    ]
+];
 
 let fabricHelper: FabricHelper;
+let client: FabricClient;
+let exampleChannel: FabricClient.Channel;
+let expectedError: Error;
+
+describe(`FabricHelper Static Functions`, () => {
+    describe(`getPeerNamesAsStringForChannel`, () => {
+        it('should return a comma delimited string of peers on a channel', () => {
+            client = new FabricClient();
+            exampleChannel = client.newChannel(EXAMPLE_CHANNEL_NAME);
+
+            exampleChannel.addPeer(examplePeer1, EXAMPLE_ORG_MSP);
+            exampleChannel.addPeer(examplePeer2, EXAMPLE_ORG_MSP);
+
+            const result = FabricHelper.getPeerNamesAsStringForChannel(
+                exampleChannel
+            );
+
+            expect(result).toBe('peer1,peer2');
+        });
+    });
+    describe(`registerAndConnectTxEventHub`, () => {
+        beforeAll(() => {
+            (FabricClient.ChannelEventHub.prototype
+                .registerTxEvent as any) = jest.fn();
+            (FabricClient.ChannelEventHub.prototype.connect as any) = jest.fn();
+
+            client = new FabricClient();
+            exampleChannel = client.newChannel(EXAMPLE_CHANNEL_NAME);
+            exampleChannel.addPeer(examplePeer1, EXAMPLE_ORG_MSP);
+
+            FabricHelper.registerAndConnectTxEventHub(exampleChannel, 'Tx1');
+        });
+        afterAll(() => {
+            jest.clearAllMocks();
+        });
+        it('should call eventHub.registerTxEvent() once', () => {
+            expect(
+                FabricClient.ChannelEventHub.prototype.registerTxEvent
+            ).toBeCalledTimes(1);
+        });
+
+        it('should call eventHub.connect() once', () => {
+            expect(
+                FabricClient.ChannelEventHub.prototype.connect
+            ).toBeCalledTimes(1);
+        });
+    });
+    describe(`inspectProposalResponses`, () => {
+        it('should return undefined when all responses are good', () => {
+            expect(
+                // Ignoring types for this to simplify the test data. Only response object inspected in function.
+                // @ts-ignore
+                FabricHelper.inspectProposalResponses(exampleProposalResponses)
+            ).toBeUndefined();
+        });
+
+        it('should throw an error if an Error is in the proposalResponses', () => {
+            const callFunc = () => {
+                FabricHelper.inspectProposalResponses(
+                    // Ignoring types for this to simplify the test data. Only response object inspected in function.
+                    // @ts-ignore
+                    exampleProposalResponsesWithError
+                );
+            };
+
+            expectedError = new Error(
+                'Failed to send instantiate/upgrade Proposal or receive valid response: Bad response'
+            );
+
+            expect(callFunc).toThrowError(expectedError);
+        });
+
+        it("should throw an error if any of the proposal responses don't have status of 200", () => {
+            const callFunc = () => {
+                FabricHelper.inspectProposalResponses(
+                    // Ignoring types for this to simplify the test data. Only response object inspected in function.
+                    // @ts-ignore
+                    exampleProposalResponsesWithBad
+                );
+            };
+
+            expectedError = new Error(
+                'Response null or has a status not equal to 200: { response: { status: 400 } }'
+            );
+
+            expect(callFunc).toThrowError(expectedError);
+        });
+    });
+    describe(`sendChaincodeProposalToPeers`, () => {
+        const exampleDeploymentOptions: any = {};
+
+        beforeAll(() => {
+            (FabricClient.Channel.prototype
+                .sendUpgradeProposal as any) = jest.fn();
+            (FabricClient.Channel.prototype
+                .sendInstantiateProposal as any) = jest.fn();
+
+            client = new FabricClient();
+            exampleChannel = client.newChannel(EXAMPLE_CHANNEL_NAME);
+        });
+
+        beforeEach(() => {
+            jest.resetAllMocks();
+        });
+
+        it('should send an upgrade proposal when the upgrade argument is true', async () => {
+            await FabricHelper.sendChaincodeProposalToPeers(
+                exampleChannel,
+                exampleDeploymentOptions,
+                true
+            );
+
+            expect(
+                FabricClient.Channel.prototype.sendUpgradeProposal
+            ).toBeCalledTimes(1);
+            expect(
+                FabricClient.Channel.prototype.sendInstantiateProposal
+            ).toBeCalledTimes(0);
+        });
+
+        it('should send an upgrade proposal when the upgrade argument is true', async () => {
+            await FabricHelper.sendChaincodeProposalToPeers(
+                exampleChannel,
+                exampleDeploymentOptions,
+                false
+            );
+
+            expect(
+                FabricClient.Channel.prototype.sendUpgradeProposal
+            ).toBeCalledTimes(0);
+            expect(
+                FabricClient.Channel.prototype.sendInstantiateProposal
+            ).toBeCalledTimes(1);
+        });
+    });
+});
 
 describe(`FabricHelper`, () => {
     beforeAll(() => {
